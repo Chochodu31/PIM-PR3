0aa86d8908ba022102c7fed78ebe7a3437db23da
Chochodu31 <chloe.migliasso@gmail.com>
2025-12-08 21:46:47 +0100

    Ajout lca + fichier gpr + routage_exceptions

--- routage_simple.adb
+++ routage_simple.adb
@@ -1,21 +1,46 @@
-with Ada.Text_IO;		use Ada.Texte_IO;
+with Ada.Text_IO;		use Ada.Text_IO;
 with Ada.Strings; 		use Ada.Strings;
 with Ada.Integer_Text_IO; 	use Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded; 	use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO;	use Ada.Text_IO.Unbounded_IO;
 with Ada.Command_Line;		use Ada.Command_Line;
 with Ada.Exceptions;		use Ada.Exceptions;
-with Sda;   use Sda;
+with LCA;
 
-procedure Routeur_simple is
+procedure Routage_simple is
+
+   type Tab_Politique is (FIFO, LRU, LFU);
+
+	type T_Adresse_IP is mod 2 ** 32;
+
+   type T_Case is record
+      Masque : T_Adresse_IP;
+      Interface : String;
+   end record;
+   
+   package LCA_routeur_simple is new LCA (
+      T_Cle => T_Adresse_IP,
+      T_Valeur => T_Case
+   );
+   use LCA_routeur_simple;
+
 	Cache : Integer;
-	type Tab_Politique is (FIFO, LRU, LFU);
 	Politique: Tab_Politique;
 	Statistique : Boolean;
 	Table : String;
 	Paquet : String;
 	Resultat : String;
 	Nb_cmd : Integer;
+   Tab_routage : T_LCA;
+	Nom_Sortie : Unbounded_String;
+	Nom_Entree : Unbounded_String;
+   Entree : File_Type;
+	Sortie : File_Type;
+   Texte : String;
+   Ligne : Integer;
+   IP_cmd : Boolean;
+   Valeur : T_Case;
+   Masque : T_Adresse_IP;
 	
 begin
    -- Comprendre la ligne de commande
@@ -61,7 +86,9 @@ begin
             Nb_cmd := Nb_cmd + 1;
             Resultat := Traiter_r(Argument(Nb_cmd), Resultat);
 
-			when others => Traiter_erreur;
+			when others =>
+            Put ("Erreur à l'argument : " & Nb_cmd);
+            raise Commande_Inconnu_Error;
 		end case;
 
 		Nb_cmd := Nb_cmd + 1;
@@ -143,10 +170,10 @@ begin
    -- Fermer les fichiers
    Close (Entree);
 	Close (Sortie);
-end Routeur_simple;
+end Routage_simple;
 
 
-function Traiter_c(Arg : in String; Cache : in out String) return String is
+function Traiter_c(Arg : in String; Cache : in out Integer) return Integer is
 begin
 	Cache := Integer(String);
 exception
---
+++ routeur_exceptions.ads
@@ -0,0 +1,4 @@
+package Routeur_exceptions is 
+   Fichier_Inconnu_Error : Exception;
+   Commande_Inconnu_Error : Exception;
+end Routeur_exceptions;
